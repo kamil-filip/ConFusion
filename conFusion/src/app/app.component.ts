8e094cb0cd8b375b7c62e084c8f9dfc3a0f51f27
kamil-filip <kamilkozlowski89@gmail.com>
2018-06-03 11:56:39 +0200

    component part 1

--- conFusion/src/app/app.component.ts
+++ conFusion/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
+//decorator
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', // if I want to have part of the screen controlled by controller
+  templateUrl: './app.component.html', //specify tmeplate file for each component
+  styleUrls: ['./app.component.scss'] //assigned styles
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
 }
--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import "hammerjs";
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
