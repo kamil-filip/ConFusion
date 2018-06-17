c4ca2d022f9d706f4eb51541b449576bc2f5fa3a
kamil-filip <kamilkozlowski89@gmail.com>
2018-06-17 09:12:00 +0200

    cool validation

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -14,14 +14,19 @@ export class DishService {
   constructor() { }
 
   getDishes(): Observable<Dish[]> {      
-    return Observable.of(DISHES).delay(2000);
+    return Observable.of(DISHES);
   }
  
   getDish(id: number): Observable<Dish> {
-    return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000);
+    return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]);
   }
 
   getFeaturedDish(): Observable<Dish>{
-    return Observable.of(DISHES.filter((dish) => (dish.featured))[0]).delay(2000);
+    return Observable.of(DISHES.filter((dish) => (dish.featured))[0]);
   }
+
+  getDishIds(): Observable<number[]> {
+    return Observable.of(DISHES.map(dish => dish.id ));
+  }
+
 }
